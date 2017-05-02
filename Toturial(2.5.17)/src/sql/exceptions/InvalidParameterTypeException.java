2c326c1c65b024acadbdfb51eaf76fe6b0cefbfa
gereon <g.t.mendler@student.utwente.nl>
2017-05-02 13:04:08 +0200

    initial project setup

---
+++ Toturial(2.5.17)/src/sql/exceptions/InvalidParameterTypeException.java
@@ -0,0 +1,14 @@
+package sql.exceptions;
+
+public class InvalidParameterTypeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8451204481971265685L;
+	
+	public InvalidParameterTypeException(String s) {
+		
+	}
+
+}
